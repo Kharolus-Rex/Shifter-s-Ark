9db84bec6947c8b41f16f09b4df50ca27fd654c2
ConnorNaylor <connor.r.naylor@gmail.com>
2023-02-22 11:19:57 -0500

    Sprint 1 Variables accomplished.

--- WinConsole/Program.cs
+++ WinConsole/Program.cs
@@ -10,6 +10,43 @@ namespace WinConsole
     {
         static void Main(string[] args)
         {
+            //basic variables for sprint 1
+            //rooms
+            string room1 = "Sewer Entrance";
+            string room2 = "Sewer Hall T";
+            string room3 = "Sewer Gang Base"; //actually a trap room
+            string room4 = "Gaol Cell"; //older spelling of jail to match setting
+            string room5 = "Gang Hallway";
+            string room6 = "Gang Office"; //the little goblin isn't a goblin
+
+            //weapons
+            string weapon1 = "greatsword";
+            string weapon2 = "longsword";
+            string weapon3 = "rapier";
+            string weapon4 = "casting focus"; //TODO screw around with spell casting if I'm feeling spicy
+            string weapon5 = "dagger";
+
+            //potions
+            string potion1 = "Minor Healing";
+            string potion2 = "Minor Invisibility";
+
+            //treasures
+            string treasure1 = "longsword +1";
+            string treasure2 = "scroll of Magic Missile";
+            string treasure3 = "scroll of ???"; //blood curse, nasty effect that triggers a set time after death. turbo zombies, basically
+
+            //items
+            string item1 = "cell key";
+            string item2 = "torch";
+            string item3 = "moldy bone"; //reference to my Growing Legend: Moldy Bone. Picked 3 high difficulty locks back-to-back with a moldy bone after getting gaoled by enemy. Expertise is borked.
+            string item4 = "archaic mark"; //tied to scroll of ???, as far as story goes
+
+            //mobs
+            string mob1 = "goblin";
+            string mob2 = "orc";
+            string mob3 = "goliath";
+            string mob4 = "goblin?"; //little too well spoken for a goblin. and quite a bloody feast it has
+            string mob5 = "barghest"; //shapeshift go brr
         }
     }
 }
