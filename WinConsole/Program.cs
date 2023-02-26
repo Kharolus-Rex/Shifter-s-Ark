94c9b4e450812a60c1ac85282fb813fc8710a614
Connor Naylor <connor.r.naylor@gmail.com>
2023-02-26 18:59:15 -0500

    Forgot name/class comment block

--- WinConsole/Program.cs
+++ WinConsole/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* *
+ * 2/26/23
+ * CSC 153
+ * Connor Naylor
+ * This will be the long time project for C# programming.
+ * This project will last at least one year if not more.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
