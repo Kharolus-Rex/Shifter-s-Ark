124ad1f05c46953619b50e74e067acdd34b3bdfe
Kharolus-Rex <connor.r.naylor@gmail.com>
2023-03-13 11:33:54 -0400

    Added LibRooms library. TODO in there to continue off of

---
+++ WinConsole/LibRooms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WinConsole
+{
+    class LibRooms
+    {
+        public static void RoomsList(string[] args)
+        {
+            // TODO Add streamreader and read text files to room variables
+        }
+    }
+}
