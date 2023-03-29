5c9709a00150e03cee10fc57177eaac4a3932d1d
Kharolus-Rex <connor.r.naylor@gmail.com>
2023-03-29 11:45:50 -0400

    simple combat mostly done. tokenization for weapons figured out. i think.

--- WinConsole/MenuLoader.cs
+++ WinConsole/MenuLoader.cs
@@ -14,7 +14,7 @@ namespace WinConsole
 
             Console.WriteLine("Welcome to the Aether, Traveler. Want to see what new world I'm crafting?\nPlease, tell me what you'd like to see or know.\n");
 
-            Console.WriteLine("1) Rooms\n2) Weapons\n3) Potions\n4) Treasures\n5) Items\n6) Mobs\n7) Leave the Aether\n");
+            Console.WriteLine("1) Rooms\n2) Weapons\n3) Potions\n4) Treasures\n5) Items\n6) Mobs\n7) Test combat\n8) Leave the Aether\n");
 
             Console.Write("Response => ");
 
@@ -47,6 +47,10 @@ namespace WinConsole
                     case "6":
                         LibMobs.MobsList();
                         break;
+
+                    case "7":
+                        SimpleCombat.simpleCombat();
+                        break;
                 }
                 Console.WriteLine("Is there anything else you'd like to see, Traveler?\n");
                 Console.Write("Response => ");
@@ -54,7 +58,7 @@ namespace WinConsole
                 response = Console.ReadLine();
             }
 
-            if (response == "7")
+            if (response == "8")
             {
                 Console.WriteLine("\nGoodbye! Next time I should have much more for you to see, Traveler!\n");
                 Console.Write("Press the Enter key to exit.");
