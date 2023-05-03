5961fa39a596cb9dab1cf19b1100b1d453b45384
Kharolus-Rex <connor.r.naylor@gmail.com>
2023-05-03 11:41:03 -0400

    Potion, Room, Treasure classes created

---
+++ WorldEngine/Classes/Potion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldEngine
+{
+    public class Potion
+    {
+        private int _idNumber;
+        private string _name;
+        private string _description;
+        private int _price;
+        private int _valChange;
+
+        public Potion (int idNumber, string name, string description, int price, int valChange)
+        {
+            IdNumber = idNumber;
+
+            Name = name;
+
+            Description = description;
+
+            Price = price;
+
+            ValChange = valChange;
+        }
+
+        public int IdNumber { get { return _idNumber; } set { _idNumber = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public int Price { get { return _price; } set { _price = value; } }
+        public int ValChange { get { return _valChange; } set { _valChange = value; } }
+    }
+}
---
+++ WorldEngine/Classes/Room.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldEngine
+{
+    public class Room
+    {
+        private int _idNumber;
+        private string _name;
+        private string _description;
+        //private Exits here
+        //private List<string> _objects;
+
+        //TODO - come back and finish room class
+
+        public Room(int idNumber, string name, string description)
+        {
+            IdNumber = idNumber;
+
+            Name = name;
+
+            Description = description;
+        }
+
+        public int IdNumber { get { return _idNumber; } set { _idNumber = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+    }
+}
