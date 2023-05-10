9bc46a96efc563f76be9f35fefc7590362128f39
Connor Naylor <connor.r.naylor@gmail.com>
2023-05-09 21:04:44 -0400

    changed from array to lists

--- WorldEngine/World.cs
+++ WorldEngine/World.cs
@@ -12,30 +12,22 @@ namespace WorldEngine
         //TODO - Convert all this into singular lists containing new Classes for each item. room list, weapon list, etc.
 
         //rooms lists
-        public static string[] rooms = new string[6];
-        public static string[] roomDescs = new string[6];
+        public static List<Room> rooms = new List<Room>();
 
         //weapons lists
-        public static string[] weapons = new string[5];
-        public static string[] weaponDmg = new string[5];
-        public static string[] weaponDescs = new string[5];
-        //public static List<Weapon> weapons = new List<Weapon>();
+        public static List<Weapon> weapons = new List<Weapon>();
 
         //treasures lists
-        public static string[] treasures = new string[3];
-        public static string[] treasureDescs = new string[3];
+        public static List<Treasure> treasures = new List<Treasure>();
 
         //potions lists
-        public static string[] potions = new string[2];
-        public static string[] potionDescs = new string[2];
+        public static List<Potion> potions = new List<Potion>();
 
         //items lists
-        public static string[] items = new string[4];
-        public static string[] itemDescs = new string[4];
+        public static List<Item> items = new List<Item>();
 
         //mobs lists
-        public static string[] mobs = new string[5];
-        public static string[] mobDescs = new string[5];
+        public static List<Mob> mobs = new List<Mob>();
 
         //current room tracker, as this will likely be used in multiple places at once. it should be outside other methods and classes
         public static int RoomTracker = 0; //initialized as 0 as that is the starting index for this list.
